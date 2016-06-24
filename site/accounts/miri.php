857fe1a303e8f060043d3d534aff2b7372ce1b66
DerZyklop <mail@der-zyklop.de>
2016-06-24 11:36:35 +0200

    added miri account

---
+++ site/accounts/miri.php
@@ -0,0 +1,10 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: miri
+firstname: Miriam
+lastname: Gückel
+email: miriamgueckel@gmx.de
+password: >
+  $2a$10$cPQbthDMj/R2Aa47RknfSed.IWITUcjT2sbEV0LwxgA48qQTpg3VS
+language: de
+role: editor
